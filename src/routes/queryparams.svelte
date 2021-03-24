0b10b90c1ba367b3195a5cd179fb7e2bdb070384
Arne Wiese <arne@wiese.me>
2021-03-24 09:12:52 +0100

    Add query params example

---
+++ src/routes/queryparams.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+  import { writable } from "svelte/store";
+
+  export async function load({ page }) {
+    const { query } = page;
+    const q = query.get("q") || "";
+    return {
+      props: {
+        q: writable(q),
+      },
+    };
+  }
+</script>
+
+<script>
+  import { onMount } from "svelte";
+  export let q = writable("");
+
+  onMount(async () => {
+    console.log("---");
+    console.log($q);
+    console.log("---");
+  });
+</script>
+
+<div class="text-center">
+  {$q}
+</div>
