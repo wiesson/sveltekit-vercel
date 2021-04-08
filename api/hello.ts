79a06454d8505b8edf5637dc92f2f2a08f77d4f7
Arne Wiese <arne@wiese.me>
2021-04-08 07:32:52 +0200

    Add comment to trigger build

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default (req: VercelRequest, res: VercelResponse) => {
+  // test
   const { name = "World" } = req.query;
   res.status(200).send(`Hello ${name}!`);
 };
